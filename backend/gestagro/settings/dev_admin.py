5a81d65136bcfbe06eddbba620f88a06ad8826ed
DevelopperGa <itdevelopper17@gmail.com>
2025-10-29 17:17:56 +0100

    feat: ajouter configuration dev_admin + security: scripts admin au gitignore

---
+++ backend/gestagro/settings/dev_admin.py
@@ -0,0 +1,76 @@
+"""
+Configuration pour Django Admin Développement
+Fonctionnalités avancées pour le développement
+"""
+from .render_local import *
+
+# Configuration spécifique au développement
+DEBUG = True
+
+# Interface de développement
+ADMIN_SITE_HEADER = "GestAgro - Administration Développement"
+ADMIN_SITE_TITLE = "GestAgro Dev"
+ADMIN_INDEX_TITLE = "Interface de Développement"
+
+# Fonctionnalités de développement
+INSTALLED_APPS += [
+    'django_extensions',
+    'debug_toolbar',
+]
+
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+# Configuration Debug Toolbar
+INTERNAL_IPS = ['127.0.0.1', 'localhost']
+
+# Logging avancé pour développement
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'logs/django_dev.log',
+            'formatter': 'verbose',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file', 'console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'gestagro': {
+            'handlers': ['file', 'console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
+# Configuration Django Extensions
+GRAPH_MODELS = {
+    'all_applications': True,
+    'group_models': True,
+}
+
+# Configuration de développement
+DEVELOPMENT_MODE = True
